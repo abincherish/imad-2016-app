3e2ea8dcb0e5cc6801bbc163000ca04eb3b460f4
abincherish <abincherish@gmail.com>
2016-09-20 19:00:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req,res) {
-    res.send('Article-one is requested and will be served here.');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function (req,res) {
@@ -25,8 +25,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/edited1 (240x320).jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'edited1 (240x320).jpg'));
 });
 
 

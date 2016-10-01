fd04cffc39f0a7f4c6b34603db9262b695cc65b8
abincherish <abincherish@gmail.com>
2016-10-01 22:34:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="new value";
 
-var img=document.getElementById("photo");
+var img=document.getElementById('photo');
 img.onClick = function () {
-    img.style.marginLeft = "100px";
+    img.style.marginLeft = '100px';
 };

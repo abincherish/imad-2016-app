766a39bc2c76574c39c9e49a38bbb1574bac00cf
abincherish <abincherish@gmail.com>
2016-10-01 21:44:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/mypic.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'mypic.jpg'));
 });

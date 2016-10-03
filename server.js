1e29608e718c7a134f799e8a2a3880cef3e679b1
abincherish <abincherish@gmail.com>
2016-10-03 19:58:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,6 +107,9 @@ app.get('/ui/mypic.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'mypic.jpg'));
 });
 
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, '/', 'favicon.ico'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {

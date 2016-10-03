eff8a5e2a00dc926ff24eec9559151aee9993253
abincherish <abincherish@gmail.com>
2016-10-03 20:25:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,6 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="new value";
 
-
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight () {

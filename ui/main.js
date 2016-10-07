172814a03866027d0e4a3dacc038dc05866114e3
abincherish <abincherish@gmail.com>
2016-10-07 21:34:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,16 @@
 
 var button = document.getElementById('button');
-var counter = 0;
 button.onClick = function () {
-    var request = new XMLHttprequest;
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML(counter.toString());
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readystate == XMLHttpRequest.DONE) {
+            if (request.status == 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML(counter.toString());
+            }
+        }
+    };
+    request.open('GET','http://abincherish.imad.hasura-app.io/counter',true);
+    request.send(null);
 };

c479f91aceec69968bcdc461dd69df92b7f9d369
abincherish <abincherish@gmail.com>
2016-10-07 20:10:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:articleName',function (req,res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
@@ -107,11 +113,6 @@ app.get('/ui/mypic.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'mypic.jpg'));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-});
 
 app.get('/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, '/', 'favicon.ico'));

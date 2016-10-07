d8d31197e28ac79ecc2fa08f6a3798756b5c24e4
abincherish <abincherish@gmail.com>
2016-10-07 21:09:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@
 var button = document.getElementById('button');
 var counter = 0;
 button.onClick = function () {
+    var request = new XMLHttprequest;
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML(counter.toString());

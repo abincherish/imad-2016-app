facb93f9a04931e6addde54536246df815a9da74
abincherish <abincherish@gmail.com>
2016-10-07 19:59:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,6 +107,12 @@ app.get('/ui/mypic.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'mypic.jpg'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, '/', 'favicon.ico'));
 });

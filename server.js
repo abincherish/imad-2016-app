e7b1cd0f7dcf9416f1bebdc292d13e12df51cb49
abincherish <abincherish@gmail.com>
2016-10-29 20:31:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,9 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/mypic.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'mypic.jpg'));
 });
-
+app.get('/ui/samplecv.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'samplecv.html'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {

311e71d9984f5b48b212acaa948337a4cd306956
abincherish <abincherish@gmail.com>
2016-11-19 00:40:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,7 +54,9 @@ var articles = {
       heading: 'Contact',
       date: 'Saturday,1st October',
       content: `<p>
-                    PETS4U is a web based application, mainly focused on to the pet farmers and pet dealers. It intends to provide support to those who are interested in pets. The shop owner who is the admin can publish the list of both currently available and new arrival pets in the shop. The system provides facilities for booking pets. In the case of advance booking of new arrival pets, the shop owner can send notifications by email and SMS when it became available.
+                    PETS4U is a web based application, mainly focused on to the pet farmers and pet dealers. It intends to provide support to those who are interested in pets. The shop owner who is the admin can publish the list of both currently available and new arrival pets in the shop. The system provides facilities for booking pets. In the case of advance booking of new arrival pets, the shop owner can send notifications by email and SMS when it became available.Users can consult any doctor, by mailing their enquiries directly to the doctors mail. The system also provides information about services of the shop, accessories, food required for pets. The farmers can enlist the pets they have for sale so that the shop owner can easily send his requirement to the farmer through this site.
+Tool  ASP.NET
+Front End  C Back End  SQL Server 2008
                 </p>`
      }
 };

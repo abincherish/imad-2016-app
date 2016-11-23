945193c43df8f4bccc05d9ecb00ff0160a9c0601
abincherish <abincherish@gmail.com>
2016-11-23 14:06:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,21 +28,9 @@ var articles = {
                 </p>
                 <p><h4>Academic Credentials</h4>
                 <table style="width:100%">
-                <tr>
-                    <th>Firstname</th>
-                    <th>Lastname</th> 
-                    <th>Age</th>
-                </tr>
-                <tr>
-                <td>Jill</td>
-                <td>Smith</td> 
-                <td>50</td>
-                </tr>
-                <tr>
-                <td>Eve</td>
-                <td>Jackson</td> 
-                <td>94</td>
-                </tr>
+                <tr><th>Degree</th><th>Institution</th><th>University/Board</th><th>Year</th><th>Percent</th></tr>
+                <tr><td>B.Tech in Computer Science and Engineering</td><td>Believers Church Caarmel Engg. College Pathanamthitta</td><td>MG University</td><td>2013-2017</td><td>73.96</td></tr>
+                <tr><td>Class XII</td><td>Mar Dionysius Sr. Secondary School Mallappally</td><td>CBSE</td>   <td>2013</td><td>70.6</td></tr>
                 </table>
                 </p>
                 

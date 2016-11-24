d75314744f9664cfec437fe2bc3b7522fc9b687b
abincherish <abincherish@gmail.com>
2016-11-24 22:35:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,10 +50,10 @@ var articles = {
                 </p>
                 <p>
                 <h4>Known Languages</h4>
-                <table style="width:100%" border="1px">
+                <table style="width:100%" border="1">
                 <tr><th></th><th>Read</th><th>Write</th><th>Speak</th></tr>
-                <tr><th>Malayalam</th><th>Y</th><th>Y</th><th>Y/th></tr>
-                <tr><th>English</th><th>Y</th><th>Y</th><th>Y/th></tr>
+                <tr><td>Malayalam</td><td>Y</td><td>Y</td><td>Y</td></tr>
+                <tr><td>English</td><td>Y</td><td>Y</td><td>Y</td></tr>
                 </table>
                 </p>
                  <p><h4>Strength and Weakness</h4>
